c30915f4213ab346309b5e95a2a69851e1428acc
João Victor S. Araujo <joaovictoraraujo.bsi@gmail.com>
2025-04-05 14:45:26 -0300

    Update exercicio02.c

--- exercicio02.c
+++ exercicio02.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
-
-void calc_uniao(int cA[50], int tA, int cB[50], int tB, int cU[50], int *tU){
-    for(int i = 0; i < tA; i++){
-        
-    for(int j = 0; j < tB; j++){
-        if(cA[i] == cB[j]){
-	        cU[*tU] = cA[i];
+void calc_inter(int cA[20], int tA, int cB[20], int tB, int cU[20], int *tU){
+    for(int h = 0; h < tA; h++){
+        cU[h] = cA[h];
+        (*tU)++;
+    }
+    for(int i = 0; i < tB; i++){
+        int aux = 0;
+        for(int j = 0; j < tA; j++){
+            if(cB[i] == cU[j]){
+                aux = 1;
+            }    
+        }
+        if(aux == 0){
+            cU[*tU] = cB[i];
             (*tU)++;
-            }
         }
     }
 }
 
-void printC(int conj[50], int tam){
+void printC(int conj[20], int tam){
     for(int i = 0; i < tam; i++){
         printf("%d", conj[i]);
+        if(i == (tam - 1)){
+            break;
+        }
+        printf(", ");
     }
     printf("\n");
 }
 
 int main(){
-    int conjA[50] = {1, 2, 3, 4, 5, 6, 7};
+    int conjA[20] = {1, 2, 3, 4, 5, 6, 7};
     int tamA = 7;
-    int conjB[50] = {2, 4, 6, 8, 10, 12, 14};
+    int conjB[20] = {2, 4, 6, 8, 10, 12, 14};
     int tamB = 7;
-    int conjU[50];
+    int conjU[20];
     int tamU = 0;
 
-    calc_uniao(conjA, tamA, conjB, tamB, conjU, &tamU);
+    calc_inter(conjA, tamA, conjB, tamB, conjU, &tamU);
 
     printC(conjA, tamA);
     printC(conjB, tamB);
